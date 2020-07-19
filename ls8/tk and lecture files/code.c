483903269d7780e58f8b728b0b4f1c952698f4c9
Jon Haase <haase1020@gmail.com>
2020-07-19 16:27:59 -0400

    initial commit with notes from tk

---
+++ ls8/tk and lecture files/code.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+int main(void)
+{
+   int x = 0b11000101;
+
+   printf("%d decimal \n", x);
+
+   printf("%x hex\n", x);
+
+   printf("%X hex\n", x);
+
+    return 0;
+}
