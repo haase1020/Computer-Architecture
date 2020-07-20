806e5595dbf2fd2a9d410df0baf3b5bcf24ad1a6
Jon Haase <haase1020@gmail.com>
2020-07-20 06:00:37 -0400

    working on day1 assignment

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,25 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256  # hold 256 bytes of memory
+        self.reg = [0] * 8  # hold 8 general-purpose registers
+        self.pc = 0
+        self.sp = 7  # stack pointer
+        self.fl = 0b00000000  # flags register is a special register
+
+    def ram_read(self, address):
+        # should accept the address to read and return the value stored there.
+        return self.ram[address]
+
+    def ram_write(self, value, address):
+        # should accept a value to write, and the address to write it to.
+        self.ram[address] = value
 
     def load(self):
         """Load a program into memory."""
@@ -15,28 +28,29 @@ class CPU:
         address = 0
 
         # For now, we've just hardcoded a program:
+        #  x = [0] * 25  # x is a list of 25 zeroes
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
+            # print("instruction", instruction)
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
-        """ALU operations."""
+        """ALU operations. ALU stands for Arithmetic Logic Unit"""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +62,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +76,32 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        LDI = 0b10000010
+        PRN = 0b01000111
+        HLT = 0b00000001
+
+        halted = False
+
+        while not halted:
+            instruction = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if instruction == LDI:
+                # set value of a register to an integer
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            elif instruction == PRN:
+                # Print numeric value stored in a given register
+                # print to the console the demial integer value that is stored in a given register
+                print(self.reg[operand_a])
+                self.pc += 2
+
+            elif instruction == HLT:
+                # halt the CPU (and exit the emulator)
+                halted = True
+
+            else:
+                print(
+                    F" unknown instruction {instruction} at address {self.pc}")
+                sys.exit()
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -8,4 +8,6 @@ from cpu import *
 cpu = CPU()
 
 cpu.load()
+# use command line arguments to open a file,
+# read in its contents line by line, and save appropriate data into RAM
 cpu.run()
