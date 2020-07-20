b3c266a0645791adb58fe3df7b65310a2f355c0b
Jon Haase <haase1020@gmail.com>
2020-07-20 15:46:13 -0400

    complete guided project lecture notes for day 1. Also achieved MVP for day 1

--- ls8/tk and lecture files/simple.py
+++ ls8/tk and lecture files/simple.py
@@ -6,8 +6,13 @@ HALT = 0b10
 PRINT_NUM = 0b00000011  # opcode 3
 SAVE = 0b100
 PRINT_REG = 0b101  # opcode 5
+ADD = 0b110
+
+# save the number 99 into R2
+# print whatever is inside R2
 
 
+# registers[2] = registers[2] + registers[3]
 memory = [
     PRINT_TIM,
     PRINT_TIM,
@@ -15,8 +20,14 @@ memory = [
     PRINT_NUM,
     42,
     SAVE,
-    2,
-    99,
+    2,  # register to save in
+    99,  # number to save
+    SAVE,
+    3,  # register to save to
+    1,  # what to save to the register
+    ADD,
+    2,  # register to look at, and save stuff in
+    3,  # register to look at
     PRINT_REG,
     2,  # register to look at
     HALT,
@@ -40,17 +51,27 @@ while running:
         running = False
 
     if command == PRINT_NUM:
-        num_to_print = memoery[pc + 1]
+        num_to_print = memory[pc + 1]
         print(num_to_print)
-
         pc += 1
+
     if command == SAVE:
-        reg = pc + 1
-        num_to_save = pc + 2
+        reg = memory[pc + 1]
+        num_to_save = memory[pc + 2]
         registers[reg] = num_to_save
 
         pc += 2
 
     if command == PRINT_REG:
+        reg_index = memory[pc + 1]
+        print(registers[reg_index])
+        pc += 1
+
+    if command == ADD:
+        first_reg = memory[pc + 1]
+        sec_reg = memory[pc + 2]
+
+        registers[first_reg] = registers[first_reg] + registers[sec_reg]
+        pc += 2
 
     pc += 1
